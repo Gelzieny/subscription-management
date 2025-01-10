b5d1bda42e4a77714c8817e1d6a2ec4cac9a20e7
Gelzieny R. Martins <gelzieny@gmail.com>
2025-01-10 18:30:16 -0300

    Finalizando projeto

--- src/views/views.py
+++ src/views/views.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, select
 from models.database import engine
 from models.model import Subscription, Payments
-from datetime import date
+from datetime import date, datetime
 
 class SubscriptionService:
   def __init__(self, engine):
@@ -20,6 +20,14 @@ class SubscriptionService:
       results = session.exec(statement).all()
     return results
   
+  def delete(self, id):
+    with Session(self.engine) as session:
+      statement = select(Subscription).where(Subscription.id==id)
+      result = session.exec(statement).one()
+
+      session.delete(result)
+      session.commit()
+      
   def _has_pay(self, results):
     for result in results:
       if result.date.month == date.today().month:
@@ -41,8 +49,58 @@ class SubscriptionService:
       session.add(pay)
       session.commit()
     
+  def total_value(self):
+    with Session(self.engine) as session:
+      statement = select(Subscription)      
+      results = session.exec(statement).all()
 
+    total = 0
+    for result in results:
+      total += result.valor
 
+    return float(total)
+
+  def _get_last_12_months_native(self):
+    today = datetime.now()
+    year = today.year
+    month = today.month
+    last_12_months = []
+    
+    for _ in range(12):
+      last_12_months.append((month, year))
+      month -= 1
+
+      if month == 0:
+          month = 12
+          year -= 1
+
+    return last_12_months[::-1]
+  
+  def _get_values_for_months(self, last_12_months):
+    with Session(self.engine) as session:
+      statement = select(Payments)
+      results = session.exec(statement).all()
+
+      value_for_months = []
+      for i in last_12_months:
+        value = 0
+
+        for result in results:
+          
+          if result.date.month == i[0] and result.date.year == i[1]:
+            value += float(result.subscription.valor)
+
+        value_for_months.append(value)
+    return value_for_months
+
+  def gen_chart(self):
+    last_12_months = self._get_last_12_months_native()
+    values_for_months = self._get_values_for_months(last_12_months)
+    last_12_months = list(map(lambda x: x[0], self._get_last_12_months_native()))
+
+    import matplotlib.pyplot as plt
+    plt.plot(last_12_months, values_for_months)
+    plt.show()
 
 ss = SubscriptionService(engine)
 # subscription = Subscription()
